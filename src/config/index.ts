a9c18761749c8fef9119abcfc4047783e7ca66a3
Artem <mstrjesm@gmail.com>
2023-11-17 15:52:10 +0200

    feat: algolia package and config

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+interface AlgoliaConfig {
+  applicationID: string;
+  apiKey: string;
+}
+
+/**
+ * Algolia configuration object.
+ * @type {AlgoliaConfig}
+ */
+export const ALGOLIA_CONFIG: AlgoliaConfig = {
+  applicationID: 'SJ8IW0NQ5X',
+  apiKey: 'dc29843971c7405f575c95719680f77d',
+}
