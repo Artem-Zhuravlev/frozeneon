f025a135603d3073b4ca24e16d1604ed3875bb6a
Artem <mstrjesm@gmail.com>
2023-11-17 15:36:00 +0200

    feat: add packages file

---
+++ src/http/packages.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosResponse } from 'axios';
+
+/**
+ * Get npm packages list.
+ *
+ * @param {string} packageName - The name of the npm package.
+ * @returns {Promise<AxiosResponse>} A promise that resolves to the packages information.
+ * @throws {Error} Throws an error if the request fails.
+ */
+
+export const getPackagesList = async (packageName: string): Promise<AxiosResponse> => {
+  try {
+    const response: AxiosResponse = await axios.get(`/package/npm/${packageName}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching package information:', error);
+    throw error;
+  }
+}
