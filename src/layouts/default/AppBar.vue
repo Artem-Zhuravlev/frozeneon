91de5740d349748de022a75fa92d8d2a12404c73
Artem <mstrjesm@gmail.com>
2023-11-17 21:03:24 +0200

    feat(app-bar): add styles, logo, input

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,62 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
-    </v-app-bar-title>
+  <v-app-bar
+    flat
+  >
+    <v-col
+      cols="5"
+      class="align-center d-flex"
+    >
+      <router-link
+        to="/"
+        class="d-inline-flex"
+      >
+        <img
+          src="@/assets/logo.svg"
+          alt="Frozeneon"
+          title="Frozeneon"
+          class="d-flex"
+          :style="{
+            maxWidth: '150px',
+            width: '100%',
+          }"
+        >
+      </router-link>
+    </v-col>
+    <v-spacer />
+    <v-col
+      md="3"
+      cols="7"
+    >
+      <v-text-field
+        v-model="searchResult"
+        density="compact"
+        variant="solo"
+        label="Search packages"
+        append-inner-icon="mdi-magnify"
+        single-line
+        hide-details
+      />
+    </v-col>
   </v-app-bar>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts">
+import { ref } from 'vue';
+
+  export default {
+    name: 'AppBar',
+    setup () {
+      const searchResult = ref('');
+
+      return {
+        searchResult
+      }
+    }
+  }
+
   //
 </script>
+
+<style>
+
+</style>
